205a8b699efb7af2545ed32a46a7ae20ad395f7c
Pedro Abreu <pedro.abreu@vilaapps.com.br>
2021-03-15 00:19:23 -0300

    Finished self-scaling application

--- variables.tf
+++ variables.tf
@@ -2,6 +2,34 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "cidr_block" {
+variable "vpc_cidr" {
   default = "192.168.0.0/16"
+}
+
+variable "public_a_cidr" {
+  default = "192.168.1.0/24"
+}
+
+variable "public_b_cidr" {
+  default = "192.168.2.0/24"
+}
+
+variable "private_a_cidr" {
+  default = "192.168.6.0/23"
+}
+
+variable "private_b_cidr" {
+  default = "192.168.4.0/23"
+}
+
+variable "ami" {
+  default = "ami-0080e4c5bc078760e"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_pair" {
+  default = "terraform"
 }
